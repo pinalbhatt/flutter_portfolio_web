5fa7ac798530459c8cc6aeec64083f1505e9f226
Pinal Bhatt <Pinal.Bhatt@ww.com>
2021-04-10 14:34:50 -0400

    commit

--- lib/desktop/desktop_view.dart
+++ lib/desktop/desktop_view.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DesktopView extends StatelessWidget {
   @override
@@ -9,26 +9,36 @@ class DesktopView extends StatelessWidget {
     // print(height.toString() + ' ' + width.toString());
     return Scaffold(
       body: SingleChildScrollView(
-              child: Column(
+        child: Column(
           children: [
             Container(
-              height: 864,
-              width: 1000,
-              child: Row(
-                children: [
+                height: 864,
+                width: 1000,
+                child: Row(children: [
                   Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('from the desk of Pinal Bhatt'),
-                      Text('PBDesk.Com'),
-                      Text('I am a software Engineer'),
-                      TextButton(onPressed: (){}, child: Text('About ME'))
-                    ]
-                  ),
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('from the desk of Pinal Bhatt',
+                            style: GoogleFonts.montserrat(fontSize: 40)),
+                        Text('PBDesk.Com',
+                            style: GoogleFonts.montserrat(fontSize: 60)),
+                        Text('I am a software Engineer',
+                            style: TextStyle(fontSize: 24)),
+                        SizedBox(height: 40),
+                        TextButton(
+                            onPressed: () {
+                              print('clicked');
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 27, horizontal: 15),
+                              child: Text('About ME',
+                                  style: TextStyle(fontSize: 20)),
+                            ))
+                      ]),
                   FlutterLogo(size: 300)
-                ]
-            )),
+                ])),
             Container(height: height, width: width, color: Colors.blue),
           ],
         ),
